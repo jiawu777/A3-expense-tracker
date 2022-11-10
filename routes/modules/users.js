9e10bb0fefb29178fd3e3485259c3a435ec41212
wujia777 <cwu005104@gmail.com>
2022-11-10 23:39:34 +0800

    feat: add alert flash message

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -16,6 +16,7 @@ router.post('/login', passport.authenticate('local', {
 
 router.get('/logout', (req, res) => {
     req.logout()
+    req.flash('success_msg', '您已成功登出!')
     res.redirect('/users/login')
 })
 
@@ -25,28 +26,32 @@ router.get('/register', (req, res) => {
 
 router.post('/register', (req, res) => {
     const { name, email, password, confirmPassword } = req.body
+    const errors = []
     if (!name || !email || !password || !confirmPassword) {
-        console.log('Every Field is required!')
-        return res.render('register', { name, email, password, confirmPassword })
+        errors.push({ message: 'Every Field is required!' })
     }
     if (password !== confirmPassword) {
-        console.log('Password & confirmPassword is different!')
-        return res.render('register', { name, email, password, confirmPassword })
+        errors.push({ message: 'Password & confirmPassword is different!' })
     }
-    User.findOne({ email })
-        .then(user => {
-            if (user) {
-                console.log('User exists.')
-                return res.render('register', { name, email, password, confirmPassword })
-            } else {
-                return User.create({ name, email, password: bcrypt.hashSync(password, bcrypt.genSaltSync(10), null) })
-                    .then(() => { res.redirect('/') })
-                    .catch(err => console.log(err))
-            }
-
+    if (errors.length) {
+        return res.render('register', {
+            errors, name, email, password, confirmPassword
         })
+    }
+    User.findOne({ email }).then(user => {
+        if (user) {
+            errors.push({ message: 'This email had been registered.' })
+            return res.render('register', {
+                errors, name, email, password, confirmPassword
+            })
+        } else {
+            return User.create({ name, email, password: bcrypt.hashSync(password, bcrypt.genSaltSync(10), null) })
+                .then(() => { res.redirect('/') })
+                .catch(err => console.log(err))
+        }
+
+    })
         .catch(err => console.log(err))
-    //待寫flash
 })
 
 
